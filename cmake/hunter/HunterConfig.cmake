bbe71f4a547590f2bbd95a6a4715c8b220135607
Kevin Scroggins <nitro404@gmail.com>
2021-07-24 12:03:35 -0400

    LibTIFF Hunterization for wxWidgets

---
+++ cmake/hunter/HunterConfig.cmake
@@ -0,0 +1,25 @@
+include_guard()
+
+hunter_config(JPEG
+	URL "https://github.com/Telefrag-Software/libjpeg/archive/refs/heads/hunter-42d15e1.zip"
+	VERSION "42d15e1"
+	SHA1 "f21d4d60517fdf58b2dff65bcec6c46f583187f7"
+)
+
+hunter_config(lzma
+	URL "https://github.com/Telefrag-Software/lzma/archive/refs/heads/hunter-2.5.3.zip"
+	VERSION "2.5.3"
+	SHA1 "c541a20b4c5bd9283d83be28b1582ce233a2683e"
+)
+
+hunter_config(ZLIB
+	URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.2.11.1.zip"
+	VERSION "1.2.11.1"
+	SHA1 "974d834f2e00dc647703c25f14617379986abd02"
+)
+
+hunter_config(zstd
+	URL "https://github.com/Telefrag-Software/zstd/archive/refs/heads/hunter-1.5.0.zip"
+	VERSION "1.5.0"
+	SHA1 "724c091ed2c6ae6e5219111a75d021c42d41b892"
+)
