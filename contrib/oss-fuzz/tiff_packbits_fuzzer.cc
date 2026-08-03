77756730616f3964dd407b8976c2b535c44d1ea4
Anthony Adame <aadame.aa@gmail.com>
2026-08-03 02:24:38 +0000

    contrib/oss-fuzz: use libtiff MIT-style license on PackBits fuzzer
    Per maintainer feedback, drop the Apache-2.0 header (which carries extra
    LICENSE/NOTICE obligations and assumes project-wide uniform licensing) in
    favour of the same MIT-style permissive license used throughout libtiff,
    under the author's own copyright rather than Sam Leffler / SGI.

--- contrib/oss-fuzz/tiff_packbits_fuzzer.cc
+++ contrib/oss-fuzz/tiff_packbits_fuzzer.cc
@@ -1,18 +1,24 @@
-// Copyright 2026 Anthony Adame
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-////////////////////////////////////////////////////////////////////////////////
+/*
+ * Copyright (c) 2026 Anthony Adame
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and
+ * its documentation for any purpose is hereby granted without fee, provided
+ * that (i) the above copyright notice and this permission notice appear in
+ * all copies of the software and related documentation, and (ii) the name of
+ * the author may not be used in any advertising or publicity relating to the
+ * software without the specific, prior written permission of the author.
+ *
+ * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND,
+ * OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER OR NOT ADVISED OF THE POSSIBILITY OF DAMAGE, AND ON ANY THEORY OF
+ * LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
 
 /*
  * Targeted fuzzer for the PackBits decoder (tif_packbits.c).
