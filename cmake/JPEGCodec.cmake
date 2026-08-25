c89f988d72dd0399745c57874a677112d3ca851f
Kevin Scroggins <nitro404@gmail.com>
2026-08-25 10:26:40 -0400

    Fix LibJPEG library dependency Linux case sensitivity issue

--- cmake/JPEGCodec.cmake
+++ cmake/JPEGCodec.cmake
@@ -29,7 +29,7 @@ set(JPEG_SUPPORT FALSE)
 
 if(HUNTER_ENABLED)
     hunter_add_package(Jpeg)
-    find_package(Jpeg CONFIG REQUIRED)
+    find_package(jpeg CONFIG REQUIRED)
 else()
     # Option to prefer standard JPEG over TurboJPEG
     option(jpeg-prefer-standard "prefer standard JPEG library over libjpeg-turbo" OFF)
